7227418dc1a15a867619fa66b3b32e6de70cdcf2
ms-lockwood <tori_lockwood@milton.edu>
2023-03-07 10:10:22 -0500

    Initial Commit

---
+++ src/ArrayWorld.java
@@ -0,0 +1,42 @@
+public class ArrayWorld {
+
+    /**ARRAY STEP ZERO
+     * DECLARE and NAME the ARRAY*/
+    public int[] numbers;
+    public double[] randomNumbers;
+
+
+        public static void main(String[] args) {
+            ArrayWorld myExample = new ArrayWorld();
+        }
+
+        public ArrayWorld(){
+        System.out.println("Arrays are lists of elements ");
+        System.out.println("Arrays index their elements starting at the number 0 ");
+
+        /**STEP ONE
+         * CONSTRUCT ARRAY and CHOOSE SET ARRAY SIZE */
+
+        numbers= new int[6];
+        randomNumbers = new double[20];
+
+        /** STEP TWO
+         * FILL THE ARRAY (Either line by line or by using a Loop)
+          */
+        //fill  numbers array
+        numbers[0]=0;
+        numbers[1]=1;
+        numbers[2]=2;
+        numbers[3]=3;
+        numbers[4]=4;
+        numbers[5]=5;
+
+        //fill randomNumbers array
+
+
+        }
+
+
+    }
+
+
